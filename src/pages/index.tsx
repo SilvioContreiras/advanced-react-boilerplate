9e0ba01c7afafb1c96ae627702d0fe3c3af1bc44
SilvioContreiras <silvio7dcontreiras@gmail.com>
2021-02-20 23:04:14 -0300

    config eslint with prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import Head from 'next/head';
 
 export default function Home() {
   return (
@@ -8,7 +8,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h3>React TS NEXT</h3>
+      <h3>React TS NEXT </h3>
     </div>
   );
 }
